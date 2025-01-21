2b50d6c7075ff17a64a7f93bfb296d52fa1fd836
piotr310100 <56043614+piotr310100@users.noreply.github.com>
2025-01-21 17:23:08 +0100

    bottle

--- examples/tutorials/bottle.py
+++ examples/tutorials/bottle.py
@@ -12,6 +12,8 @@ scene = gs.Scene(
         substeps=10,
     ),
     mpm_options=gs.options.MPMOptions(
+        # lower_bound=(-0.5, -1.0, 0.0),
+        # upper_bound=(0.5, 1.0, 1),
         lower_bound=(-1.0, -1.0, -1.0),
         upper_bound=(1.0, 1.0, 1),
     ),
@@ -30,18 +32,37 @@ plane = scene.add_entity(
     morph=gs.morphs.Plane(pos=(-1, -1, -1)),
 )
 
+
+import os
+materials = {
+    "base": gs.materials.MPM.Base,
+    "elastic": gs.materials.MPM.Elastic,
+    "elastoplastic": gs.materials.MPM.ElastoPlastic,
+    "liquid": gs.materials.MPM.Liquid,
+    "muscle": gs.materials.MPM.Muscle,
+    "sand": gs.materials.MPM.Sand,
+    "snow": gs.materials.MPM.Snow,
+}
+
+MATERIAL="elastic"
+MODEL_PATH="output\\bottle"
+OBJ_PATH=os.path.join(MODEL_PATH, "pseudomesh_info\\ours_30000\\scale_1.obj")
+SAVE_PATH=os.path.join(MODEL_PATH, "genesis_triangles", MATERIAL)
+os.makedirs(SAVE_PATH, exist_ok=True)
 SCALE=0.4
 mesh = scene.add_entity(
     material=gs.materials.MPM.Elastic(sampler="random"),#(E=1e5, nu=0.1, rho=1000),
-    morph=gs.morphs.Mesh(file="scale_1.obj", convexify=False, decompose_nonconvex=False, scale=SCALE)
+    morph=gs.morphs.Mesh(file=OBJ_PATH, convexify=False, decompose_nonconvex=False, scale=SCALE)
 )
 
 scene.build()
 
 ########################## build ##########################
 
-
-horizon = 1000
+horizon = 200
 for i in range(horizon):
     scene.step()
-    print(torch.tensor(mesh.get_state().pos[mesh.particle_start:mesh.particle_end]).shape)
+    saved_tensor = torch.tensor(mesh.get_state().pos[mesh.particle_start:mesh.particle_end].clone().detach().to("cpu")).reshape(-1, 3, 3)
+    torch.save(saved_tensor, os.path.join(SAVE_PATH, f"{i:05d}.pt"))
+    del saved_tensor
+    torch.cuda.empty_cache()
